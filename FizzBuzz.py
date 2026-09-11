a3a54b70df6523f9aa33b4b969f9d98d6b5bdf5e
Jesusway69 Mac mini macOS <jesusway69@gmail.com>
2026-09-11 23:07:47 +0200

    20260911 Add clean screen an format text on FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,24 +1,24 @@
-import os
-os.system('clear')
+from subprocess import run
+from platform import platform
+
+run("cls" if platform().startswith("Windows") else "clear", shell=True)
 
 """Escribe un programa que muestre por consola (con un print) los
-  #números de 1 a 100 (ambos incluidos y con un salto de línea entre
-  cada impresión), sustituyendo los siguientes:
+  números de 1 a 100 (ambos incluidos en horizontal
+  y con un salto de línea cada 10 impresiones)
+  , sustituyendo los siguientes:
   - Múltiplos de 3 por la palabra "fizz".
   - Múltiplos de 5 por la palabra "buzz".
- - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz" """
+  - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz" """
 
-
-for i in range(1,101):
-    if i % 3 == 0 and i % 5 == 0:
-        print ("FizzBuzz")
+for i in range(1, 101):
+    if i % 15 == 0:
+        print(f"{'FizzBuzz':^10}", end="")
     elif i % 5 == 0:
-        print ("Buzz")
+        print(f"{'Buzz':^10}", end="")
     elif i % 3 == 0:
-        print ("Fizz")
+        print(f"{'Fizz':^10}", end="")
     else:
-        print (i)
-        
-
-     
-
+        print(f"{i:^10}", end="")
+    if i % 10 == 0:
+        print()
